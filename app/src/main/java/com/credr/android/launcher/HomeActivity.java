9a6fd9134f44465e9a0f5192f9f639fab2fcff97
vijay-credr <vijaykumar@credr.in>
2015-10-19 20:35:36 +0530

    Disabling status bar and adding icons for wifi/data/gps

--- app/src/main/java/com/credr/android/launcher/HomeActivity.java
+++ app/src/main/java/com/credr/android/launcher/HomeActivity.java
@@ -2,18 +2,23 @@ package com.credr.android.launcher;
 
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageButton;
@@ -34,12 +39,15 @@ public class HomeActivity extends Activity {
     ImageButton infoView;
     SharedPreferences sharedPreferences;
     FragmentManager fragmentManager;
+    WindowManager wManager;
+    CustomViewGroup lockView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if(Utils.isLockingModeActive(HomeActivity.this)) lock(this);
         appGridView = (GridView)findViewById(R.id.appGrid);
         infoView = (ImageButton) findViewById(R.id.infoView);
         loadApps();
@@ -54,7 +62,8 @@ public class HomeActivity extends Activity {
                     infoView.setImageDrawable(getResources().getDrawable(R.drawable.launcher_icon, getTheme()));
                     editor.putBoolean(Utils.PREF_LOCKING_MODE, true);
                     editor.commit();
-                    Toast.makeText(HomeActivity.this, "Please set launcher as default for home and settings immediately", Toast.LENGTH_SHORT).show();
+                    lock(HomeActivity.this);
+                    Toast.makeText(HomeActivity.this, "Please set launcher as default for home immediately", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -102,8 +111,20 @@ public class HomeActivity extends Activity {
                     public void onClick(View v) {
                         Intent i = manager.getLaunchIntentForPackage(getItem(position).name.toString());
                         if(getItem(position).data != null) {
-                            i = new Intent(HomeActivity.this, WebViewActivity.class);
-                            i.putExtra(WebViewActivity.URL_EXTRA, getItem(position).data);
+                            String label = getItem(position).label.toString();
+                            if(label.contains("WiFi")) {
+                                startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                                return;
+                            } else if(label.contains("Data")) {
+                                startActivity(new Intent(Settings.ACTION_APN_SETTINGS));
+                                return;
+                            } else if(label.contains("GPS")) {
+                                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                                return;
+                            } else {
+                                i = new Intent(HomeActivity.this, WebViewActivity.class);
+                                i.putExtra(WebViewActivity.URL_EXTRA, getItem(position).data);
+                            }
                         }
                         HomeActivity.this.startActivity(i);
                     }
@@ -165,7 +186,28 @@ public class HomeActivity extends Activity {
         appInfo.label = "CredR Homepage";
         appInfo.name = "com.android.chrome";
         appInfo.data = "http://www.credr.com";
-        appInfo.icon = getResources().getDrawable(android.R.drawable.ic_menu_mapmode,getTheme());
+        appInfo.icon = getResources().getDrawable(R.drawable.credr,getTheme());
+        appInfoList.add(appInfo);
+
+        appInfo = new AppInfo();
+        appInfo.label = "WiFi Settings";
+        appInfo.name = "com.android.chrome";
+        appInfo.data = "http://www.credr.com";
+        appInfo.icon = getResources().getDrawable(R.drawable.wifi,getTheme());
+        appInfoList.add(appInfo);
+
+        appInfo = new AppInfo();
+        appInfo.label = "Data Settings";
+        appInfo.name = "com.android.chrome";
+        appInfo.data = "http://www.credr.com";
+        appInfo.icon = getResources().getDrawable(R.drawable.apn,getTheme());
+        appInfoList.add(appInfo);
+
+        appInfo = new AppInfo();
+        appInfo.label = "GPS Settings";
+        appInfo.name = "com.android.chrome";
+        appInfo.data = "http://www.credr.com";
+        appInfo.icon = getResources().getDrawable(R.drawable.gps,getTheme());
         appInfoList.add(appInfo);
     }
 
@@ -193,6 +235,52 @@ public class HomeActivity extends Activity {
         LoginFragment loginFragment = (LoginFragment)fragmentManager.findFragmentByTag(LoginFragment.LOGIN_FRAGMENT_TAG);
         if(loginFragment!=null && loginFragment.isVisible()) {
             loginFragment.dismiss();
+        }
+    }
+
+    private class CustomViewGroup extends ViewGroup {
+        private Context context;
+
+        public CustomViewGroup(Context context) {
+            super(context);
+            this.context = context;
+        }
+
+        @Override
+        protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        }
+        @Override
+        public boolean onInterceptTouchEvent(MotionEvent ev) {
+            return true;
+        }
+    }
+
+    public void lock(Activity activity) {
+
+        //lock top notification/status bar
+        wManager = ((WindowManager) activity.getApplicationContext()
+                .getSystemService(Context.WINDOW_SERVICE));
+
+        WindowManager.LayoutParams topBlockParams = new WindowManager.LayoutParams();
+        topBlockParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+        topBlockParams.gravity = Gravity.TOP;
+        topBlockParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+        topBlockParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        topBlockParams.height = (int) (50 * activity.getResources()
+                .getDisplayMetrics().scaledDensity);
+        topBlockParams.format = PixelFormat.TRANSPARENT;
+
+        lockView = new CustomViewGroup(activity);
+        wManager.addView(lockView, topBlockParams);
+    }
+
+    public void unlock() {
+        if (lockView!=null) {
+            if (lockView.isShown()) {
+                wManager.removeView(lockView);
+            }
         }
     }
 }
--- app/src/main/java/com/credr/android/launcher/Utils/Utils.java
+++ app/src/main/java/com/credr/android/launcher/Utils/Utils.java
@@ -11,6 +11,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashSet;
@@ -23,6 +26,8 @@ public class Utils {
 
     public static final String PREF_LOCKING_MODE = "LOCKING_MODE";
     public static final String PREF_ACCESS_LIST = "ACCESS_LIST";
+    public static final int AID_APP = 10000;
+    public static final int AID_USER = 100000;
 
     public static String loadPreferencesJSONFromAsset(Context context) {
         String json = null;
@@ -84,5 +89,106 @@ public class Utils {
                 (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         return netInfo != null && netInfo.isConnected();
+    }
+
+    public static String getForegroundApp() {
+        File[] files = new File("/proc").listFiles();
+        int lowestOomScore = Integer.MAX_VALUE;
+        String foregroundProcess = null;
+
+        for (File file : files) {
+            if (!file.isDirectory()) {
+                continue;
+            }
+
+            int pid;
+            try {
+                pid = Integer.parseInt(file.getName());
+            } catch (NumberFormatException e) {
+                continue;
+            }
+
+            try {
+                String cgroup = read(String.format("/proc/%d/cgroup", pid));
+
+                String[] lines = cgroup.split("\n");
+
+                if (lines.length != 2) {
+                    continue;
+                }
+
+                String cpuSubsystem = lines[0];
+                String cpuaccctSubsystem = lines[1];
+
+                if (!cpuaccctSubsystem.endsWith(Integer.toString(pid))) {
+                    // not an application process
+                    continue;
+                }
+
+                if (cpuSubsystem.endsWith("bg_non_interactive")) {
+                    // background policy
+                    continue;
+                }
+
+                String cmdline = read(String.format("/proc/%d/cmdline", pid));
+
+                if (cmdline.contains("com.android.systemui")) {
+                    continue;
+                }
+
+                int uid = Integer.parseInt(
+                        cpuaccctSubsystem.split(":")[2].split("/")[1].replace("uid_", ""));
+                if (uid >= 1000 && uid <= 1038) {
+                    // system process
+                    continue;
+                }
+
+                int appId = uid - AID_APP;
+                int userId = 0;
+                // loop until we get the correct user id.
+                // 100000 is the offset for each user.
+                while (appId > AID_USER) {
+                    appId -= AID_USER;
+                    userId++;
+                }
+
+                if (appId < 0) {
+                    continue;
+                }
+
+                // u{user_id}_a{app_id} is used on API 17+ for multiple user account support.
+                // String uidName = String.format("u%d_a%d", userId, appId);
+
+                File oomScoreAdj = new File(String.format("/proc/%d/oom_score_adj", pid));
+                if (oomScoreAdj.canRead()) {
+                    int oomAdj = Integer.parseInt(read(oomScoreAdj.getAbsolutePath().trim()));
+                    if (oomAdj != 0) {
+                        continue;
+                    }
+                }
+
+                int oomscore = Integer.parseInt(read(String.format("/proc/%d/oom_score", pid)));
+                if (oomscore < lowestOomScore) {
+                    lowestOomScore = oomscore;
+                    foregroundProcess = cmdline;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return foregroundProcess;
+    }
+
+    private static String read(String path) throws IOException {
+        StringBuilder output = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        output.append(reader.readLine());
+        for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+            output.append('\n').append(line);
+        }
+        reader.close();
+        return output.toString();
     }
 }
