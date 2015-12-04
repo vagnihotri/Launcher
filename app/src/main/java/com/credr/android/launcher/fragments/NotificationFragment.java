26bd9738fe51f50468d7daae0927e7eb831c7b5d
vijay-credr <vijaykumar@credr.in>
2015-12-04 18:50:15 +0530

    Adding basic notification support

--- app/src/main/java/com/credr/android/launcher/fragments/NotificationFragment.java
+++ app/src/main/java/com/credr/android/launcher/fragments/NotificationFragment.java
@@ -1,11 +1,13 @@
 package com.credr.android.launcher.fragments;
 
 import android.app.DialogFragment;
+import android.app.PendingIntent;
 import android.os.Bundle;
 import android.service.notification.StatusBarNotification;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.RemoteViews;
 
 import com.credr.android.launcher.R;
@@ -23,13 +25,33 @@ public class NotificationFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View cView = inflater.inflate(R.layout.notification_layout, null);
-        RemoteViews remoteViews =  new RemoteViews(getActivity().getPackageName(), R.layout.notification_layout);
+        RelativeLayout containerView = (RelativeLayout)cView.findViewById(R.id.notifcnContainer);
         List<StatusBarNotification> notificationList = NotificationStore.getInstance().getNotifications();
-        for(StatusBarNotification statusBarNotification : notificationList) {
+        int index = Integer.MAX_VALUE;
+        for(final StatusBarNotification statusBarNotification : notificationList) {
             if(statusBarNotification.getNotification() != null) {
                 RemoteViews remoteView = statusBarNotification.getNotification().contentView;
-                remoteViews.addView(R.id.notifcnContainer, remoteView);
+                View notfView = remoteView.apply(getActivity(), container);
+                notfView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        try {
+                            statusBarNotification.getNotification().contentIntent.send();
+                            NotificationStore.getInstance().removeNotification(statusBarNotification);
+                            NotificationFragment.this.dismiss();
+                        } catch (PendingIntent.CanceledException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                notfView.setId(index);
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                if(index < Integer.MAX_VALUE ) {
+                    params.addRule(RelativeLayout.BELOW, index + 1);
+                }
+                containerView.addView(notfView, params);
             }
+            index--;
         }
         return cView;
     }
