fcad2c11ab571caa5ac8b0840c92ba4fdf4c0864
vijay-credr <vijaykumar@credr.in>
2015-12-11 19:38:31 +0530

    Adding crashlytics

--- app/src/main/java/com/credr/android/launcher/CredRLauncherApplication.java
+++ app/src/main/java/com/credr/android/launcher/CredRLauncherApplication.java
@@ -2,12 +2,15 @@ package com.credr.android.launcher;
 
 import android.content.Intent;
 
+import com.crashlytics.android.Crashlytics;
 import com.credr.android.launcher.Utils.Utils;
 import com.credr.android.launcher.service.AppListenerService;
 import com.credr.android.library.CredRApplication;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by vijayagnihotri on 25/09/15.
@@ -17,7 +20,7 @@ public class CredRLauncherApplication extends CredRApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        //Fabric.with(this, new Crashlytics());
+        Fabric.with(this, new Crashlytics());
         startAppListenerService();
         loadPreferences();
     }
